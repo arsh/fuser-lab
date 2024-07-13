6fbdbb502569f4d2477f64968041e075c93efe9d
Andres Santana <hernaa@amazon.com>
2024-07-13 12:55:11 +0000

    adding simple file system

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use clap::*;
+use fuser::MountOption;
+use simple::SimpleFS;
+
+mod simple;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("hello")
+        .version(crate_version!())
+        .author("Christopher Berner")
+        .arg(
+            Arg::new("MOUNT_POINT")
+                .required(true)
+                .index(1)
+                .help("Act as a client, and mount FUSE at given path"),
+        )
+        .arg(
+            Arg::new("auto_unmount")
+                .long("auto_unmount")
+                .action(ArgAction::SetTrue)
+                .help("Automatically unmount on process exit"),
+        )
+        .arg(
+            Arg::new("allow-root")
+                .long("allow-root")
+                .action(ArgAction::SetTrue)
+                .help("Allow root user to access filesystem"),
+        )
+        .get_matches();
+    env_logger::init();
+    let mountpoint = matches.get_one::<String>("MOUNT_POINT").unwrap();
+    let mut options = vec![MountOption::RO, MountOption::FSName("hello".to_string())];
+    if matches.get_flag("auto_unmount") {
+        options.push(MountOption::AutoUnmount);
+    }
+    if matches.get_flag("allow-root") {
+        options.push(MountOption::AllowRoot);
+    }
+    fuser::mount2(SimpleFS, mountpoint, &options).unwrap();
 }
